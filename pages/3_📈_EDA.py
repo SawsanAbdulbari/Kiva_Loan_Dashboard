84ab8052a4885b72dceaa27aa32da94024b89e19
SawsanAbdulbari <sawsan23000@student.hamk.fi>
2023-10-24 19:44:17 +0300

    Updated conflicted files

--- pages/3_📈_EDA.py
+++ pages/3_📈_EDA.py
@@ -22,13 +22,10 @@ df2 = pd.read_csv(r'C:\Users\sawsa_v1iymt5\OneDrive\Documents\data_science\kiva_
 
 # Sidebar for additional information
 st.sidebar.title('Kiva Dashboard')
-<<<<<<< HEAD
+
 image = "kuva.png"
 st.sidebar.image(image, caption='Image', use_column_width=True)
-=======
-image = Image.open(r"C:\Users\sawsa_v1iymt5\Downloads\My-Github\Kiva_Loan_Dashboard\kuva.png")
-st.sidebar.image(image)
->>>>>>> fb8056f97c026462ee014060033206eb0294e461
+
 st.sidebar.write("This dashboard is using Kiva dataset from Kaggle for Python Data Analysis Diploma.")
 st.sidebar.write("")
 
